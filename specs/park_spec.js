c757e242c4a8d1a38661f69c157889649a64406c
Angus Malloch <angusbass@hotmail.com>
2018-04-03 18:52:05 +0100

    Added test and method to add dinosaur to an enclosure.

--- specs/park_spec.js
+++ specs/park_spec.js
@@ -12,4 +12,18 @@ describe('Park', function () {
     dinosaur1 = new Dinosaur({ type: 'Parasaurolophus', offspring_per_year: 8});
   });
 
+  // enclosure should start empty
+  it('enclosure should start empty', function() {
+    assert.strictEqual(park.enclosure.length, 0);
+  });
+
+  // should be able to add dinosaur
+  it('can add dinosaur to enclosure', function(){
+    park.addDinosaur(dinosaur1);
+    assert.strictEqual(park.enclosure.length, 1)
+  })
+
+  // should be able to remove all dinosaurs of a particular type
+  // should get all the dinosaurs with an offspring count of more than 2
+
 });
